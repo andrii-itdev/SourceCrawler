2b35e428f27a2ad198dd18c49b7720796d6c6465
andrii-itdev <andrii.itdev@gmail.com>
2024-01-23 18:24:36 +0100

    added several components for user interraction

--- SourceCrawler/src/app/app.module.ts
+++ SourceCrawler/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserInputFormComponent } from './user-input-form/user-input-form.component';
+import { ViewDashboardComponent } from './view-dashboard/view-dashboard.component';
+import { ViewSourceComponent } from './view-source/view-source.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputFormComponent,
+    ViewDashboardComponent,
+    ViewSourceComponent
   ],
   imports: [
     BrowserModule,
